f22d20a93c337fc516bbe2742d962dd44ea3379e
Shun Matsumoto <legend.of.sheiosu@gmail.com>
2020-03-26 20:41:23 +0900

    若干関数名を修正しました

--- www/model/functions.php
+++ www/model/functions.php
@@ -136,5 +136,5 @@ function is_valid_upload_image($image){
 }
 
 function h($escape) {
-  return htmlspecialchars($s, ENT_QUOTES, 'UTF-8');
+  return htmlspecialchars($escape, ENT_QUOTES, 'UTF-8');
 }
