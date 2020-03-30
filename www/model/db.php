75b4b158ccdd7e60b405a51adb95ff4184479c48
Shun Matsumoto <legend.of.sheiosu@gmail.com>
2020-03-30 21:22:47 +0900

    #4 admin_viewに対し、クロスサイトスクリプティングの対策を実装しました

--- www/model/db.php
+++ www/model/db.php
@@ -1,5 +1,6 @@
 <?php
 
+// データベースに接続する関数
 function get_db_connect(){
   // MySQL用のDSN文字列
   $dsn = 'mysql:dbname='. DB_NAME .';host='. DB_HOST .';charset='.DB_CHARSET;
@@ -7,8 +8,11 @@ function get_db_connect(){
   try {
     // データベースに接続
     $dbh = new PDO($dsn, DB_USER, DB_PASS, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8mb4'));
+    // 例外を投げる
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // ネイティブのプリペアドステートメントを使おうとする
     $dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    // 結果セットに返された際のカラム名と添字をつけた配列を返す
     $dbh->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
   } catch (PDOException $e) {
     exit('接続できませんでした。理由：'.$e->getMessage() );
@@ -20,7 +24,9 @@ function fetch_query($db, $sql, $params = array()){ //1行ずつ配列して返�
   try{
     //sql実行の準備
     $statement = $db->prepare($sql);
+    //sql文の実行
     $statement->execute($params);
+    //レコードの取得
     return $statement->fetch();
   }catch(PDOException $e){
     set_error('データ取得に失敗しました。'); //エラーメッセージ表示
