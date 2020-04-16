f6f243da532f894b97ec0ad71f2b9f100e5f14e7
Shun Matsumoto <legend.of.sheiosu@gmail.com>
2020-04-16 13:19:05 +0900

    #9 append_schema.sqlのスペルミス、updatedの修正を行いました

--- lamp_dock/append_schema.sql
+++ lamp_dock/append_schema.sql
@@ -10,8 +10,8 @@
 
 CREATE TABLE `orders` (
   `order_id` int(11) NOT NULL AUTO_INCREMENT,
-  `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAM,
-  `updated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -21,8 +21,8 @@ CREATE TABLE `order_details` (
   `item_id` int(11) NOT NULL,
   `order_price` int(11) NOT NULL,
   `item_amount` int(11) NOT NULL,
-  `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `updated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- 第2正規化(itemsとして既に存在する)
